4168e752e044f0907e835556502d114c629e4b7c
LiangcaiChen <chenl47@cardiff.ac.uk>
2016-12-20 14:49:46 +0000

    add right and left key controllerfor selecting zombie

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	private int selectedZombiePosotion = 0;
 	public GameObject selectedZombie;
 	public List<GameObject> zombies;
 	public Vector3 selectedSize;
@@ -17,7 +18,36 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown ("left")) {
+			GetZombieLeft ();
+		}
+
+		if (Input.GetKeyDown ("right")) {
+
+		}
+
+		if (Input.GetKeyDown ("up")) {
+
+		}
+
+	}
+
+	void GetZombieLeft() {
+		if (selectedZombiePosotion == 0) {
+			SelectZombie (zombies [3]);
+		} else {
+			GameObject newZombie = zombies[selectedZombiePosotion - 1];
+			SelectZombie (newZombie);
+		}
+	}
+
+	void GetZombieRight() {
+		if (selectedZombiePosotion == 3) {
+			SelectZombie (zombies [0]);
+		} else {
+			GameObject newZombie = zombies[selectedZombiePosotion + 1];
+			SelectZombie (newZombie);
+		}
 	}
 
 	void SelectZombie(GameObject newZombie) {
