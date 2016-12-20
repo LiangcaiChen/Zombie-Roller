9819d9439cd040378954b096914321e15ac19585
LiangcaiChen <chenl47@cardiff.ac.uk>
2016-12-20 14:30:40 +0000

    add floor and 4 zombies in the game then add a script to set selected size

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+
+	// Use this for initialization
+	void Start () {
+		SelectZombie (selectedZombie);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SelectZombie(GameObject newZombie) {
+		newZombie.transform.localScale = selectedSize;
+	}
+}
+
